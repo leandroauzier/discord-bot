05d42a37dc1ca036e2e54ddd7492a2857524be0d
Leonardo Henrique <leonardoh481@gmail.com>
2022-05-05 16:33:26 -0300

    Adding reference to db_string

--- env_search.py
+++ env_search.py
@@ -6,6 +6,7 @@ DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 DISCORD_GUILD = os.getenv('DISCORD_GUILD')
 SECRET_KEY_DJANGO = os.getenv('SECRET_KEY_DJANGO')
 BEARER_PUBLIC_API= os.getenv('BEARER_PUBLIC_API')
+DB_STRING= os.getenv('DB_STRING')
 
 HOST= os.getenv('HOST')
 USER= os.getenv('USER')
