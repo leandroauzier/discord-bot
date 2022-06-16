6d60e33d9b2f741bed0aef340aa86e64c6031f86
leandroauzier <leandroauzier02@gmail.com>
2022-06-16 15:13:10 -0300

    modifing bot commands

--- discord_bot.py
+++ discord_bot.py
@@ -4,9 +4,6 @@ import os
 import logging
 from env_search import DISCORD_TOKEN
 from discord.ext import commands
-from public_api import list_of_collections, list_of_transactions, info_from_collection
-from query import Set_collections_tb, get_collection_from_acronyms_id, Set_acronyms, get_from_api
-from vips import svg_conversion
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
@@ -15,24 +12,23 @@ client = commands.Bot(command_prefix='!')
 client.remove_command('help')
 
 @client.command()
-async def oi(ctx, *, arg):
-    texto = f"Olá! você disse:\n> {arg}"
-    await ctx.channel.send(texto)
+async def oi(ctx):
+    await ctx.channel.send(f"Olá!, {ctx.message.author.mention}")
 
 async def on_ready():
     print(f'Bot está pronto para uso!')
     
 @client.command()
 async def help(ctx, *arg):
-        embed = discord.Embed(title="Blackcup bot Help", color=0xA7F3D0)
+        embed = discord.Embed(title="Blackcup bot Help", color=0x4b286c)
         embed.description = ("**__Comandos do Bot__**:\n\n"
-        "**oi:**\n!oi\n"                             
-        "**Help:**\n!help\n"
-        "**Regras:**\n!rules\n")
+        "**O bot irá lhe mandar um olá!:**\n!oi\n"                             
+        "**Help/ajuda:**\n!help\n"
+        "**Regras do server:**\n!regras\n")
         await ctx.channel.send(embed=embed)
 @client.command()
 async def regras(ctx, *arg):
-        embed = discord.Embed(title="", color=0xA7F3D0)
+        embed = discord.Embed(title="", color=0x4b286c)
         embed.description=(f":rotating_light: Nossas regras de conduta do servidor: :rotating_light:\n\n1-Seja respeitoso\n" # noqa:
                                 f"2- O envio de qualquer material nocivo, como malwares ou harmwares, resulta em uma Ban imediata e permanente.\n" # noqa:
                                 f"3- Pode falar em Português-Brasil ou Inglês mas não faça spam.\n"
@@ -51,7 +47,7 @@ async def on_member_join(self, member):
     guild = member.guild
     if guild.system_channel is not None:
         ctx = f'{member.mention} acabou de entrar em {guild.name}'
-        embed = discord.Embed(title="Blackcup bot Help", color=0xA7F3D0)
+        embed = discord.Embed(title="Blackcup bot Help", color=0x4b286c)
         embed.description = ("__Comandos do Bot__:\n\n"
         "**oi:**\n!oi\n"
         "**Help:**\n!help\n"
